5f47cf2de81b916856f1bc217a33b78879b280a8
Anderson Araujo <andersonaraujoc1@gmail.com>
2022-07-08 17:54:03 -0300

    Finalizando Projeto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+import "./MoviesGrid.css";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -6,19 +9,29 @@ const apiKey = import.meta.env.VITE_API_KEY;
 const Home = () => {
   const [topMovies, setTopMovies] = useState([]);
 
-  const getTopRateMovies = async (url) => {
+  const getTopRatedMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log(data);
+    setTopMovies(data.results);
   };
 
   useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    console.log(topRatedUrl);
+    getTopRatedMovies(topRatedUrl);
+  }, []);
 
-    const topRateUrl = ;
-  }, [])
-
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.lenght === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
